61744f125f13dbcd63640cdfe1ada9d62085556f
Vanshika Anand <vanshika@piggy.co.in>
2020-01-29 16:31:47 +0530

    search page

--- views/Search/components/Card.js
+++ views/Search/components/Card.js
@@ -8,13 +8,6 @@ import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
   icon: {
     color: "rgba(255, 255, 255, 0.54)"
   }
